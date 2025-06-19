ee0ff3d784e7938f7e76a843a4d1ff65eb765048
Shenan Vindinu <shenanvindinu@gmail.com>
2025-06-19 17:48:15 +0530

    Luigi's skill level reduced to 55

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
 Route::get('/ninjas', function () {
     $ninjas = [
         ["name" => "mario", "skill" => 75, "id" => "1"],
-        ["name" => "luigi", "skill" => 88, "id" => "2"]
+        ["name" => "luigi", "skill" => 55, "id" => "2"]
     ];
 
     return view('ninjas.index', ["greeting" => "Hello", "ninjas" => $ninjas]);
