78feebe6a898d802375f737572321a6570040278
Shenan Vindinu <shenanvindinu@gmail.com>
2025-06-20 18:32:49 +0530

    Delete Ninjas Created

--- resources/views/ninjas/show.blade.php
+++ resources/views/ninjas/show.blade.php
@@ -16,4 +16,11 @@
     <p>{{ $ninja->dojo->description }}</p>
   </div>
 
+  <form action="{{ route('ninjas.destroy', $ninja)}}" method="POST">
+    @csrf
+    @method('DELETE')
+
+    <button type="submit" class="btn my-4">Delete Ninja</button>
+  </form>
+
 </x-layout>    
