433f4ccc3773db5d71552dfe554f867037892d3e
Shenan Vindinu <shenanvindinu@gmail.com>
2025-06-20 09:49:48 +0530

    vite annotaion and class names added to html

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ninja Network</title>
+    @vite('resources/css/app.css')
 </head>
-<body>
+<body class="text-center px-8 py-12">
 
     <h1>Welcome to the Shenan's Ninja Network</h1>
     <p>Click the button to view the list of ninjas</p>
 
-    <a href="/ninjas" class="btn">
+    <a href="/ninjas" class="btn mt-4 inline-block">
         Find Ninjas!
     </a>
     
